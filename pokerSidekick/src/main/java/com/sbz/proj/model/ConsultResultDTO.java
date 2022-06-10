c440ecda37c5f60f23fb2727455e4c7979929de8
isidora-stanic <isidora.stanic@outlook.com>
2022-06-10 22:49:34 +0200

    Added dto for better consulting

---
+++ pokerSidekick/src/main/java/com/sbz/proj/model/ConsultResultDTO.java
@@ -0,0 +1,14 @@
+package com.sbz.proj.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ConsultResultDTO {
+    public String action, explanation;
+}
